3d50a524f3640419cca17e617f5715561ed51695
juhayerayaz <aiazj97@gmail.com>
2022-12-27 22:32:47 +0600

    all slider fixed

--- src/Components/BlogSlider/BlogCard.js
+++ src/Components/BlogSlider/BlogCard.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BsPlusCircle } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
-const BlogCard = ({ blogimg }) => {
+const BlogCard = ({ blogimg, date, title, description, link }) => {
     const [background, setBackground] = useState('')
     const [zoom, setZoom] = useState('')
     const [icon, setIcon] = useState('')
@@ -35,9 +35,9 @@ const BlogCard = ({ blogimg }) => {
                 </div>
             </div>
             <div className='px-8 pt-3 pb-8'>
-                <p className='text-[#888888] text-base mb-2'>Announcement,DLSH <span className='text-[#0097FA] text-3xl'>-</span> March 1,2022</p>
-                <Link className='font-bold text-lg' to="2022/01/31/donate-love-spread-happiness-2022/">Donate Love, Spread Happiness 2022</Link>
-                <p className='text-[#888888] text-sm mt-4'>১৪ই ফেব্রুয়ারির এই দিনটা একটু অন্যভাবে চিন্তা করুন। এই দিনটি যদি অসহায় পরিবারহীন শিশুদের সাথে আনন্দ ভাগাভাগি করে কাটাতে পারেন, মাতৃস্নেহ দিয়ে একবেলা খাবার খাইয়ে দিতে পারেন, বাবা-মায়ের মতো ভালোবেসে</p>
+                <p className='text-[#888888] text-base mb-2'>Announcement,DLSH <span className='text-[#0097FA] text-3xl'>-</span> {date}</p>
+                <Link className='font-bold text-lg' to={link}>{title}</Link>
+                <p className='text-[#888888] text-sm mt-4'>{description}</p>
             </div>
         </div>
     );
--- src/Components/BlogSlider/BlogSlider.js
+++ src/Components/BlogSlider/BlogSlider.js
@@ -11,12 +11,28 @@ const BlogSlider = () => {
     const data = {
         navigation: false,
         pagination: false,
-        autoplay: true,
+        autoplay: false,
         slidesPerView: 3
     }
     const blogimg = {
         img1: blog1,
         img2: blog2
+    }
+    const date = {
+        date1: ' March 1,2022',
+        date2: ' January 31,2022'
+    }
+    const title = {
+        title1: 'Donate Love, Spread Happiness 2022',
+        title2: 'ভালোবাসা দিবসে পথশিশুদের পাশে ওয়ার্কস ফর হিউমিনিটি ফাউন্ডেশন'
+    }
+    const description = {
+        description1: '১৪ই ফেব্রুয়ারির এই দিনটা একটু অন্যভাবে চিন্তা করুন। এই দিনটি যদি অসহায় পরিবারহীন শিশুদের সাথে আনন্দ ভাগাভাগি করে কাটাতে পারেন, মাতৃস্নেহ দিয়ে একবেলা খাবার খাইয়ে দিতে পারেন, বাবা-মায়ের মতো ভালোবেসে',
+        description2: 'ভালোবাসা দিবসে যেখানে সবাই তাদের পরিবার পরিজন,প্রিয় মানুষের সাথে ভালোবাসা দিবস উদযাপন করে সেখানে সমাজ কথিত সুবিধাবঞ্চিত পথশিশু সবার মাঝে খুশি ছড়িয়ে দিতে দিনব্যাপী নানা কার্যক্রমের মাধ্যমে বিশ্বসাহিত্য কেন্দ্রে ভালোবাসা'
+    }
+    const link = {
+        link1: '2022/01/31/donate-love-spread-happiness-2022/',
+        link2: '/2022/03/01/dlsh2022/',
     }
     return (
         <div className='mx-3 lg:mx-40 my-20'>
@@ -33,21 +49,37 @@ const BlogSlider = () => {
                 <SwiperSlide>
                     <BlogCard
                         blogimg={blogimg.img1}
+                        date={date.date1}
+                        title={title.title1}
+                        description={description.description1}
+                        link={link.link1}
                     />
                 </SwiperSlide>
                 <SwiperSlide>
                     <BlogCard
                         blogimg={blogimg.img2}
+                        date={date.date2}
+                        title={title.title2}
+                        description={description.description2}
+                        link={link.link2}
                     />
                 </SwiperSlide>
                 <SwiperSlide>
                     <BlogCard
                         blogimg={blogimg.img1}
+                        date={date.date1}
+                        title={title.title1}
+                        description={description.description1}
+                        link={link.link1}
                     />
                 </SwiperSlide>
                 <SwiperSlide>
                     <BlogCard
                         blogimg={blogimg.img2}
+                        date={date.date2}
+                        title={title.title2}
+                        description={description.description2}
+                        link={link.link2}
                     />
                 </SwiperSlide>
             </Slider>
