1c58ed0711c65b0b22745b9dc2b2aad84ec24c6d
Nafis Abdullah <n.ahmad.web.cit22@gmail.com>
2022-12-27 19:12:38 +0600

    Carousel updated

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -53,7 +53,54 @@ const Banner = () => {
               DONATE NOW!
             </a>
           </div>
-          <div className="absolute bottom-0 left-0 w-full h-[4vw] bg-black blur-2xl"></div>
+        </div>
+        <div
+          className="relative w-screen bg-cover bg-no-repeat font-pop"
+          style={{
+            background: "url('assets/images/banner/banner_main.jpg')",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="w-full h-full bg-black/50 text-white font-bold px-12 md:px-20 lg:px-20 py-56 text-center">
+            <h1 className="font-semibold text-4xl md:text-5xl lg:text-6xl mb-6 animate-[slideY_1s_ease_1]">
+              Small Efforts Make Big Change
+            </h1>
+            <p className="font-normal text-lg md:text-xl lg:text-2xl w-3/4 md:w-2/3 lg:w-[38%] mx-auto mb-8 animate-[slideY_1.3s_ease_1]">
+              We make a living by what we get but we make a life by what we
+              give!
+            </p>
+            <a
+              href="#"
+              className="inline-block linear duration-300 py-3 md:py-5 text-white text-lg lg:text-2xl px-7 md:px-10 bg-primary/60 hover:bg-primary/80 animate-[slideY_1.8s_ease_1]"
+            >
+              DONATE NOW!
+            </a>
+          </div>
+        </div>
+        <div
+          className="relative w-screen bg-cover bg-no-repeat font-pop"
+          style={{
+            background: "url('assets/images/banner/banner_main.jpg')",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="w-full h-full bg-black/50 text-white font-bold px-12 md:px-20 lg:px-20 py-56 text-center">
+            <h1 className="font-semibold text-4xl md:text-5xl lg:text-6xl mb-6 animate-[slideY_1s_ease_1]">
+              Small Efforts Make Big Change
+            </h1>
+            <p className="font-normal text-lg md:text-xl lg:text-2xl w-3/4 md:w-2/3 lg:w-[38%] mx-auto mb-8 animate-[slideY_1.3s_ease_1]">
+              We make a living by what we get but we make a life by what we
+              give!
+            </p>
+            <a
+              href="#"
+              className="inline-block linear duration-300 py-3 md:py-5 text-white text-lg lg:text-2xl px-7 md:px-10 bg-primary/60 hover:bg-primary/80 animate-[slideY_1.8s_ease_1]"
+            >
+              DONATE NOW!
+            </a>
+          </div>
         </div>
       </Carousel>
       <BannerFooter />
