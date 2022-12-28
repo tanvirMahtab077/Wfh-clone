e1291df5f1fb95ad04ac20096ac1a01a32dbae1d
juhayerayaz <aiazj97@gmail.com>
2022-12-28 17:02:38 +0600

    fixed error in responsiveness

--- src/components/SideStat/SideStat.js
+++ src/components/SideStat/SideStat.js
@@ -6,16 +6,16 @@ import { GiProgression } from 'react-icons/gi'
 const SideStat = ({ statHead, statTitle, statDescription }) => {
     return (
         <div>
-            <div className='mb-20 flex items-center justify-between'>
-                <div>
+            <div className='mb-20 lg:flex items-center justify-between'>
+                <div className='mb-14 lg:mb-5'>
                     <div className='flex items-center mb-5'>
-                        <span className='border border-t-0 border-r-0 border-b-0 border-[#0097FA] py-6  bg-[#0097FA] mr-3'></span>
+                        <span className='border border-t-0 border-r-0 border-b-0 border-[#0097FA] py-7 lg:py-6  bg-[#0097FA] mr-3'></span>
                         <div>
                             <h1 className='text-[#0097FA] text-lg font-semibold'>{statHead}</h1>
                             <h1 className='font-bold text-3xl font-sans'>{statTitle}</h1>
                         </div>
                     </div>
-                    <p className='text-sm text-zinc-400 w-[680px]'>{statDescription}</p>
+                    <p className='text-sm text-zinc-400 lg:w-[680px]'>{statDescription}</p>
                 </div>
                 <div className='mr-28'>
                     <div className='text-[#fec501] font-semibold text-2xl flex items-center mb-10'>
