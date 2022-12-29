cc557e1eca5f6e710cd130c3416eb925eeb42248
Nafis Abdullah <n.ahmad.web.cit22@gmail.com>
2022-12-30 00:47:25 +0600

    blog banner added

--- src/pages/blogs/Blog.js
+++ src/pages/blogs/Blog.js
@@ -2,12 +2,13 @@ import React from "react";
 import Banner from "../../components/banner/Banner";
 import BlogShowcase from "../../components/blogShowcase/BlogShowcase";
 import BlogSideBar from "../../components/blogSideBar/BlogSideBar";
+import HeroBanner from "../../components/HeroBanner/HeroBanner";
 
 const Blog = () => {
   return (
     <>
-    
-      <div className="max-w-container mx-auto flex flex-col lg:flex-row justify-start gap-x-7 mt-12">
+      <HeroBanner header={"WFH Blogs"} />
+      <div className="max-w-container mx-auto flex flex-col lg:flex-row justify-start gap-x-7 mt-8">
         <BlogShowcase className={"w-full lg:w-3/4"} />
         <BlogSideBar className={"w-full lg:w-1/4"} />
       </div>
