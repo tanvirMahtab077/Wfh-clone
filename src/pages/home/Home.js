c347d4146b55f3dcb2a02931e3db039a647cbaff
juhayerayaz <aiazj97@gmail.com>
2022-12-31 15:27:23 +0600

    reach and presense component added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,14 +8,19 @@ import BlogSlider from "../../components/BlogSlider/BlogSlider";
 import '../../pages/home/Home.css';
 import DiscoverBanner from '../../components/discoverBanner/DiscoverBanner';
 import HelpBanner from '../../components/helpBanner/HelpBanner';
+import ReachPresense from '../../components/reachPresense/ReachPresense';
 
 
 const Home = () => {
+  const statHead = "Lorem Ipsum Is Simply Dummy Text"
+  const statTitle = "Reach And Presence"
+  const statDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has"
   return (
     <>
       <Banner />
       <DonationCards></DonationCards>
       <Announcement></Announcement>
+      <ReachPresense statHead={statHead} statTitle={statTitle} statDescription={statDescription} />
       <HelpBanner />
       <ActivitySlider />
       <DiscoverBanner />
