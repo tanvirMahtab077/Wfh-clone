76333215a2e9cd283fcec4d1dc988d851933f238
juhayerayaz <aiazj97@gmail.com>
2023-01-16 00:55:52 +0600

    json added

---
+++ src/components/search.js
@@ -0,0 +1,211 @@
+export const search_data = [
+    {
+        id: "1",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "2",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "3",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "4",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "5",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "6",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "7",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "8",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "9",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "10",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "11",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "12",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "13",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "14",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "15",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "16",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "17",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "18",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "19",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "20",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "21",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "22",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "23",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "24",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "25",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+    {
+        id: "",
+        img: "",
+        title: "",
+        description: "",
+        link: "",
+        date: ""
+    },
+]
+export default search_data
