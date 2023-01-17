e5a5e2c42cd64819ba3f6c45942b8103cb62cf5f
Nafis Abdullah <n.ahmad.web.cit22@gmail.com>
2023-01-17 13:10:02 +0600

    updated

--- src/components/search.js
+++ src/components/search.js
@@ -1,211 +1,217 @@
-export const search_data = [
-    {
-        id: "1",
-        img: "https://wfhbd.org/wp-content/uploads/2023/01/comming-soon-logo-750x269.png",
-        title: "cl3",
-        description: "",
-        link: "#",
-        date: "January 13, 2023"
-    },
-    {
-        id: "2",
-        img: "https://wfhbd.org/wp-content/uploads/2023/01/comming-soon-logo-750x269.png",
-        title: "cl2",
-        description: "",
-        link: "#",
-        date: "January 13, 2023"
-    },
-    {
-        id: "3",
-        img: "https://wfhbd.org/wp-content/uploads/2023/01/comming-soon-logo-750x269.png",
-        title: "",
-        description: "",
-        link: "#",
-        date: "January 13, 2023"
-    },
-    {
-        id: "4",
-        img: "https://wfhbd.org/wp-content/uploads/2023/01/27857782_587255074953756_1534691068294016208_n-703x550.jpg",
-        title: "Donate Love, Spread Happiness 2018",
-        description: "‘নিজের কস্টগুলো অন্যের সাথে ‘ভাগাভাগি” করলে কস্ট কমে”আর ‘ভালোবাসা ভাগাভাগি করলে, ভালোবাসা” বেড়ে যায়।এই কথাটা আমি",
-        link: "/wfh-blogs/donate-love-spread-happiness-2018",
-        date: "January 13, 2023"
-    },
-    {
-        id: "5",
-        img: "https://wfhbd.org/wp-content/uploads/2023/01/272148987_1490696454664564_6860631045042577687_n-750x499.jpg",
-        title: "Donate Love, Spread Happiness 2019",
-        description: "ভালবাসা দিবসে নারায়ণগঞ্জের সোনারগাঁ উপজেলার পিরোজপুর এলাকায় অবস্থিত পিরোজপুর ইসলামিয়া আরাবিয়া মাদ্রসার প্রায় ১৪০ জন গরীব",
-        link: "/wfh-blogs/donate-love-spread-happiness-2022",
-        date: "January 13, 2023"
-    },
-    {
-        id: "6",
-        img: "https://wfhbd.org/wp-content/uploads/2023/01/DLSH-1-750x500.jpg",
-        title: "Donate Love, Spread Happiness 2022",
-        description: "ভালোবাসা দিবসে যেখানে সবাই তাদের পরিবার পরিজন,প্রিয় মানুষের সাথে ভালোবাসা দিবস উদযাপন করে সেখানে সমাজ কথিত",
-        link: "/wfh-blogs/ভালোবাসা-দিবসে-পথশিশুদের-পাশে-ওয়ার্কস-ফর-হিউমিনিটি-ফাউন্ডেশন",
-        date: "January 13, 2023"
-    },
-    {
-        id: "7",
-        img: "",
-        title: "Donate Love, Spread Happiness 2018",
-        description: "‘নিজের কস্টগুলো অন্যের সাথে ‘ভাগাভাগি” করলে কস্ট কমে”আর ‘ভালোবাসা ভাগাভাগি করলে, ভালোবাসা” বেড়ে যায়।এই কথাটা আমি",
-        link: "/wfh-blogs/donate-love-spread-happiness-2018",
-        date: "January 13, 2023"
-    },
-    {
-        id: "8",
-        img: "",
-        title: "Donate Love, Spread Happiness 2019",
-        description: "ভালবাসা দিবসে নারায়ণগঞ্জের সোনারগাঁ উপজেলার পিরোজপুর এলাকায় অবস্থিত পিরোজপুর ইসলামিয়া আরাবিয়া মাদ্রসার প্রায় ১৪০ জন গরীব",
-        link: "/wfh-blogs/donate-love-spread-happiness-2022",
-        date: "January 13, 2023"
-    },
-    {
-        id: "9",
-        img: "https://wfhbd.org/wp-content/uploads/2023/01/DLSH-1-750x500.jpg",
-        title: "Donate Love, Spread Happiness 2023",
-        description: "",
-        link: "/dlsh",
-        date: "January 13, 2023"
-    },
-    {
-        id: "10",
-        img: "",
-        title: "Header",
-        description: "",
-        link: "#",
-        date: "December 11, 2022"
-    },
-    {
-        id: "11",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "12",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "13",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "14",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "15",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "16",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "17",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "18",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "19",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "20",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "21",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "22",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "23",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "24",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "25",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-    {
-        id: "",
-        img: "",
-        title: "",
-        description: "",
-        link: "",
-        date: "January 13, 2023"
-    },
-]
-export default search_data
+const search_data = [
+  {
+    id: "1",
+    img: "https://wfhbd.org/wp-content/uploads/2023/01/comming-soon-logo-750x269.png",
+    title: "cl3",
+    description: "",
+    link: "#",
+    date: "January 13, 2023",
+  },
+  {
+    id: "2",
+    img: "https://wfhbd.org/wp-content/uploads/2023/01/comming-soon-logo-750x269.png",
+    title: "cl2",
+    description: "",
+    link: "#",
+    date: "January 13, 2023",
+  },
+  {
+    id: "3",
+    img: "https://wfhbd.org/wp-content/uploads/2023/01/comming-soon-logo-750x269.png",
+    title: "",
+    description: "",
+    link: "#",
+    date: "January 13, 2023",
+  },
+  {
+    id: "4",
+    img: "https://wfhbd.org/wp-content/uploads/2023/01/27857782_587255074953756_1534691068294016208_n-703x550.jpg",
+    title: "Donate Love, Spread Happiness 2018",
+    description:
+      "‘নিজের কস্টগুলো অন্যের সাথে ‘ভাগাভাগি” করলে কস্ট কমে”আর ‘ভালোবাসা ভাগাভাগি করলে, ভালোবাসা” বেড়ে যায়।এই কথাটা আমি",
+    link: "/wfh-blogs/donate-love-spread-happiness-2018",
+    date: "January 13, 2023",
+  },
+  {
+    id: "5",
+    img: "https://wfhbd.org/wp-content/uploads/2023/01/272148987_1490696454664564_6860631045042577687_n-750x499.jpg",
+    title: "Donate Love, Spread Happiness 2019",
+    description:
+      "ভালবাসা দিবসে নারায়ণগঞ্জের সোনারগাঁ উপজেলার পিরোজপুর এলাকায় অবস্থিত পিরোজপুর ইসলামিয়া আরাবিয়া মাদ্রসার প্রায় ১৪০ জন গরীব",
+    link: "/wfh-blogs/donate-love-spread-happiness-2022",
+    date: "January 13, 2023",
+  },
+  {
+    id: "6",
+    img: "https://wfhbd.org/wp-content/uploads/2023/01/DLSH-1-750x500.jpg",
+    title: "Donate Love, Spread Happiness 2022",
+    description:
+      "ভালোবাসা দিবসে যেখানে সবাই তাদের পরিবার পরিজন,প্রিয় মানুষের সাথে ভালোবাসা দিবস উদযাপন করে সেখানে সমাজ কথিত",
+    link: "/wfh-blogs/ভালোবাসা-দিবসে-পথশিশুদের-পাশে-ওয়ার্কস-ফর-হিউমিনিটি-ফাউন্ডেশন",
+    date: "January 13, 2023",
+  },
+  {
+    id: "7",
+    img: "",
+    title: "Donate Love, Spread Happiness 2018",
+    description:
+      "‘নিজের কস্টগুলো অন্যের সাথে ‘ভাগাভাগি” করলে কস্ট কমে”আর ‘ভালোবাসা ভাগাভাগি করলে, ভালোবাসা” বেড়ে যায়।এই কথাটা আমি",
+    link: "/wfh-blogs/donate-love-spread-happiness-2018",
+    date: "January 13, 2023",
+  },
+  {
+    id: "8",
+    img: "",
+    title: "Donate Love, Spread Happiness 2019",
+    description:
+      "ভালবাসা দিবসে নারায়ণগঞ্জের সোনারগাঁ উপজেলার পিরোজপুর এলাকায় অবস্থিত পিরোজপুর ইসলামিয়া আরাবিয়া মাদ্রসার প্রায় ১৪০ জন গরীব",
+    link: "/wfh-blogs/donate-love-spread-happiness-2022",
+    date: "January 13, 2023",
+  },
+  {
+    id: "9",
+    img: "https://wfhbd.org/wp-content/uploads/2023/01/DLSH-1-750x500.jpg",
+    title: "Donate Love, Spread Happiness 2023",
+    description: "",
+    link: "/dlsh",
+    date: "January 13, 2023",
+  },
+  {
+    id: "10",
+    img: "",
+    title: "Header",
+    description: "",
+    link: "#",
+    date: "December 11, 2022",
+  },
+  {
+    id: "11",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "12",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "13",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "14",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "15",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "16",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "17",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "18",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "19",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "20",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "21",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "22",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "23",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "24",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "25",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+  {
+    id: "",
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+    date: "January 13, 2023",
+  },
+];
+
+export default search_data;
